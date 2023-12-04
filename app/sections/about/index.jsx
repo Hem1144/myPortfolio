405a3a6c841e7fef1f426083f21ac69773e42280
Hem1144 <hemlaldulal.986@gmail.com>
2023-12-05 00:05:30 +0545

    fix: fixed files for production

---
+++ app/sections/about/index.jsx
@@ -0,0 +1,90 @@
+"use client";
+
+import { useRef } from "react";
+import { LazyMotion, domAnimation, useInView } from "framer-motion";
+import { HeadingDivider } from "components";
+import { TimeLine } from "./TimeLine";
+
+export function AboutSection() {
+	const ref = useRef(null);
+	const isInView = useInView(ref, { once: true });
+
+	return (
+		<LazyMotion features={domAnimation}>
+			{/* <section id="about" className="section">
+				<HeadingDivider title="About Me" />
+				<div className="pt-10 pb-16 max-w-5xl flex flex-col gap-3">
+					<div
+						tabIndex="0"
+						ref={ref}
+						className="text-xl font-light leading-relaxed"
+						style={{
+							transform: isInView ? "none" : "translateX(-200px)",
+							opacity: isInView ? 1 : 0,
+							transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
+						}}
+					>
+						<p>
+							My name is Vasile. I successfully graduated Technical University of Moldova, computer
+							science faculty.
+						</p>
+						<p>
+							During my university years, I developed a deep passion for web programming, and I
+							consistently engaged in learning new concepts and techniques in this field.
+						</p>
+						<p>So, officially, my programming journey began in 2017 as a WordPress developer.</p>
+						<p className="my-3.5">
+							I enjoy learning technologies that interest me, which is why I dedicated a significant
+							amount of time to working with WordPress.
+						</p>
+						<p>
+							One day, I decided to try something new, and I started learning React. I was so
+							excited to see how fast I can build a website with it, and how easy it is to maintain
+							it. Consequently, I quickly learnt Next.js and I used it to build some company
+							projects working in a team with a backend dev.
+						</p>
+						<p className="my-3.5">
+							In december 2023, I had an opportunity to start working as a React developer for a
+							company with the possibility of transitioning to mobile development. I was skeptical
+							about it at first, but I decided to give it a try, and I don&lsquo;t regret it. Now, I
+							thoroughly enjoy working with Flutter and Dart.
+						</p>
+					</div>
+				</div>
+
+				<TimeLine />
+			</section> */}
+			<div className="  grid sm:grid-cols-2  gap-y-20 sm:gap-y-0">
+				<div className=" border-l-[2px]  px-8   border-[#44B24A]">
+					<div>
+						<h2 className="  font-bold text-2xl">My Experience</h2>
+						<div className=" mt-5 border-l-[2px] px-5   border-[#44B24A]">
+							<h3 className=" text-xl  font-semibold">MERN full Stack Development</h3>
+							<span className=" text-sm  text-green-200">
+								{" "}
+								Deerwalk Training Center / March 2022 - June 2022
+							</span>
+						</div>
+						<div className=" mt-5 border-l-[2px] px-5   border-[#44B24A]">
+							<h3 className=" text-xl  font-semibold">Full Stack Development / Trainee</h3>
+							<span className=" text-sm  text-green-200"> TEJ Center / June 2023 - Present</span>
+						</div>
+					</div>
+				</div>
+
+				<div className=" border-l-[2px] h-[5rem] px-8   border-[#44B24A]">
+					<div>
+						<h2 className="  font-bold text-2xl">My Education</h2>
+						<div className=" mt-5 border-l-[2px] h-[5rem] px-5   border-[#44B24A]">
+							<h3 className=" text-xl  font-semibold">
+								Bachlor of Science in Computer Science and Information Technology(Bsc.CSIT)
+							</h3>
+							<span className=" text-sm  text-green-200"> Patan Multipal Campus / 2018 - 2022</span>
+						</div>
+					</div>
+				</div>
+				<div></div>
+			</div>
+		</LazyMotion>
+	);
+}
---
+++ app/sections/project/index.jsx
@@ -0,0 +1,107 @@
+import { Suspense, useRef } from "react";
+import { domAnimation, LazyMotion, useInView } from "framer-motion";
+import Slider from "react-slick";
+import Link from "next/link";
+import useSWR from "swr";
+import { HeadingDivider, Loader } from "components";
+import { fetcher } from "utils/fetcher";
+import Error from "../../error";
+import { ErrorBoundary } from "react-error-boundary";
+import { Projects } from "../../projects/components/Projects";
+import { SITE_ROUTES } from "../../../constants";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const url = `${process.env.NEXT_PUBLIC_SANITY_URL}${process.env.NEXT_PUBLIC_SANITY_LATEST_PROJECTS}`;
+
+export function ProjectsSection() {
+	var settings = {
+		dots: true,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 2,
+		margin: 10,
+		slidesToScroll: 1,
+		// autoplay: true, // Add autoplay option
+		autoplaySpeed: 3000, // Set the autoplay speed in milliseconds (adjust as needed)
+		responsive: [
+			{
+				breakpoint: 768, // Set a breakpoint where you want to change the number of slides shown
+				settings: {
+					slidesToShow: 1 // Set the number of slides to show at this breakpoint
+				}
+			}
+			// Add more breakpoints and settings as needed
+		]
+	};
+
+	const projects = [
+		{
+			title: "E-Commerce",
+			src: "../s.png",
+			toolbar: ["React", "Next JS", "Redex JS", "Tailwind"],
+			link: "https://www.salesberry.com.np/"
+		},
+		{
+			title: "RealState",
+			src: "../lal1.png",
+			toolbar: ["React", "Next JS", "Redex JS", "CSS", "Bootstrap"],
+			link: "https://lalpurja.vercel.app/"
+		},
+
+		{
+			title: "FoodMenu",
+			src: "../fo.png",
+			toolbar: ["React", "Next JS", "Redex JS", "Tailwind"],
+			link: "https://food-hithub-io.vercel.app/"
+		},
+		{
+			title: "IdeaForNepal",
+			src: "../ide.png",
+			toolbar: ["React", "Next JS", "Redex JS", "Tailwind"],
+			link: "https://ideafornepal.com/"
+		}
+	];
+
+	return (
+		<LazyMotion features={domAnimation}>
+			<section id="projects" className="section">
+				<HeadingDivider title="My Projects" />
+				<div className="h-10 md:h-14" />
+
+				<Slider {...settings}>
+					{projects.map((item, i) => (
+						<div key={i}>
+							<div className="border-green-500 sm:mx-10 bg-[#2D2C3B] rounded-tr-[100px] rounded-bl-[100px] overflow-hidden p-12 hover:bg-green-100 transform duration-300 shadow-2xl">
+								<div className="bg-black">
+									<img src={item.src} alt={item.title} />
+								</div>
+								<h3 className="text-green-500 text-center font-bold text-2xl mt-10">
+									E-{item.title}
+								</h3>
+								<div className="flex justify-center my-10">
+									<div className="flex gap-4 ">
+										{item.toolbar.map((data, i) => (
+											<span key={i} className="bg-[#484A59] p-2 rounded-lg">
+												{data}
+											</span>
+										))}
+									</div>
+								</div>
+								<div className="flex justify-between items-center">
+									<div className="flex justify-center w-full">
+										<Link href={item.link}>
+											<button className="border-[2px] border-green-500 text-green-500 rounded-lg px-4 py-2 w-full">
+												Live Preview
+											</button>
+										</Link>
+									</div>
+								</div>
+							</div>
+						</div>
+					))}
+				</Slider>
+			</section>
+		</LazyMotion>
+	);
+}

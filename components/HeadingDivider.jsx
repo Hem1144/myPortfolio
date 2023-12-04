401c28b804fa9081556382a5f5b3d8dcd319b1d2
Hem1144 <hemlaldulal.986@gmail.com>
2023-12-04 18:06:27 +0545

    feat: Added required files to buld portfolio website

---
+++ components/HeadingDivider.jsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { LazyMotion, domAnimation, m } from "framer-motion";
+import { initial, animate, exit, transition } from "utils/motions";
+
+export function HeadingDivider({ title = "" }) {
+	return (
+		<header className="flex items-center">
+			<LazyMotion features={domAnimation}>
+				<m.h2
+					tabIndex="0"
+					initial={initial}
+					animate={animate}
+					exit={exit}
+					transition={transition}
+					className="heading-divider  shadow-2xl"
+				>
+					{!title.includes("Project") ? (
+						<>
+							<span className=" text-[#44B24A]">About Skills</span>
+						</>
+					) : (
+						<>
+							My <span className=" text-[#44B24A]">Project</span>
+						</>
+					)}
+				</m.h2>
+			</LazyMotion>
+		</header>
+	);
+}
---
+++ constants/strings.js
@@ -0,0 +1,6 @@
+export const SITE_STRINGS = {
+	textLogo: "@Hemlal Dulal",
+	backToMainPageTitle: "Back to main page",
+	goToMainPageTitle: "Go to main page",
+	backToMainText: "Back to main"
+};
